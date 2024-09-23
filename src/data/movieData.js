7c0c599464a8ef40fd6c7e2559556d5b7261ddb0
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-09-23 10:47:21 +0300

    Simple corrections

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -1,7 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 
-const dbPath = path.resolve('./src/db.json') ;
+const dbPath = path.resolve('./src/db.json');
 
 async function getDb() {
     const db = await fs.readFile(dbPath, { encoding: 'utf-8' });
