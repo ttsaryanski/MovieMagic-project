b6a8d65212c9ec4da856ed3590640dedd23d73bd
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-10-02 19:45:43 +0300

    Simple changes

--- src/config/mongooseInit.js
+++ src/config/mongooseInit.js
@@ -1,5 +1,6 @@
 import { connect } from 'mongoose';
 
+const LocalDB_URL = 'mongodb://localhost:27017/';
 const CloudDB_URL = 'mongodb+srv://SoftUniUser:SoftUniBackEndSeptember2024@softuni.a3ekn.mongodb.net/';
 
 export default async function mongooseInit() {
