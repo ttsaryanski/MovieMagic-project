a4b9fd640b4bf9a2eca0c90ac247dd1ab646e8fe
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-10-03 18:41:39 +0300

    Update mongooseInit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This project is part of the course JS BackEnd of SoftUni!
 
-Set connect to CloudDB_URL in config/mongooseInit for testing!
+<!-- Set connect to CloudDB_URL in config/mongooseInit for testing! -->
--- src/config/mongooseInit.js
+++ src/config/mongooseInit.js
@@ -2,10 +2,11 @@ import { connect } from 'mongoose';
 
 const LocalDB_URL = 'mongodb://localhost:27017/';
 const CloudDB_URL = 'mongodb+srv://SoftUniUser:SoftUniBackEndSeptember2024@softuni.a3ekn.mongodb.net/';
+const DB_URL = LocalDB_URL || CloudDB_URL;
 
 export default async function mongooseInit() {
     try {
-        await connect(LocalDB_URL, { dbName: 'MovieMagic'});
+        await connect(DB_URL, { dbName: 'MovieMagic'});
     
         console.log('Successfully connect to DB!');
         
