49fd3c6709f9397fca69f33a81779d8111ab6364
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-10-10 19:50:27 +0300

    Changes in mongooseInit

--- src/config/mongooseInit.js
+++ src/config/mongooseInit.js
@@ -1,17 +1,24 @@
 import { connect } from 'mongoose';
 
-const LocalDB_URL = 'mongodb://localhost:27017/';
+const LocalDB_URL = 'mongodb://localhost:2701/';
 const CloudDB_URL = 'mongodb+srv://SoftUniUser:SoftUniBackEndSeptember2024@softuni.a3ekn.mongodb.net/';
-const DB_URL = LocalDB_URL || CloudDB_URL;
 
 export default async function mongooseInit() {
     try {
-        await connect(DB_URL, { dbName: 'MovieMagic'});
+        await connect(LocalDB_URL, { dbName: 'MovieMagic'});
     
-        console.log('Successfully connect to DB!');
-        
+        console.log('Successfully connect to local DB!');  
     } catch (error) {
-        console.log("Failed to connect to DB!");
+        console.log("Failed to connect to local DB!");
         console.log(error.message);
+
+        try {
+            await connect(CloudDB_URL, { dbName: 'MovieMagic'});
+    
+            console.log('Successfully connect to cloud DB!');
+        } catch (error) {
+            console.log("Failed to connect to cloud DB!");
+            console.log(error.message);
+        }
     };
 };
